a546d9109f27dd44529039c15c001dabcfa08df1
eradoria <eradoria@gmail.com>
2022-07-25 20:57:40 -0500

    3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My name is erick
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isClicked: true,
+      inputValue: "",
+      listofTodos: [],
+    };
+  }
+
+  handleClick = () => {
+    this.state.isClicked
+      ? this.setState({ isClicked: false })
+      : this.setState({ isClicked: true });
+  };
+
+  handleChange = (e) => {
+    this.setState({ inputValue: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      listofTodos: [...this.state.listofTodos, this.state.inputValue],
+    });
+    this.setState.inputValue({ inputValue: "" });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              value={this.state.inputValue}
+              onChange={this.handleChange}
+            ></input>
+            <button type="submit">submit here</button>
+          </form>
+          <ol>
+            {this.state.listofTodos.map((todo, index) => {
+              return <li key={index}>{todo}</li>;
+            })}
+          </ol>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
